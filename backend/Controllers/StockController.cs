2affe8c9fa69bd32a1b6c6f8761aed7688194a46
916RasnitaRadu <rasnitaradu@gmail.com>
2026-02-15 14:23:18 +0200

    added crud for stocks

---
+++ backend/Controllers/StockController.cs
@@ -0,0 +1,95 @@
+﻿using backend.Data;
+using backend.DTOs;
+using backend.Mappers;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList()
+                .Select(s => s.ToStockDto()).ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStock([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock.ToStockDto());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStock([FromBody] CreateStockRequestDto stockRequest)
+        {
+            var stockModel = stockRequest.ToStock();
+
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+
+            // returns a 201 CREATED code & shows the location where you can find the result
+            return CreatedAtAction(
+                nameof(GetStock),
+                new { id = stockModel.Id },
+                stockModel.ToStockDto()
+            );
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStock([FromRoute] int id, [FromBody] CreateStockRequestDto stockRequest)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            stockModel.CompanyName = stockRequest.CompanyName;
+            stockModel.Purchase = stockRequest.Purchase;
+            stockModel.Symbol = stockRequest.Symbol;
+            stockModel.Industry = stockRequest.Industry;
+            stockModel.LastDiv = stockRequest.LastDiv;
+            stockModel.MarketCap = stockRequest.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto());
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStock([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ backend/Mappers/StockMapper.cs
@@ -0,0 +1,49 @@
+﻿using backend.DTOs;
+using backend.Models;
+using System.Collections;
+
+namespace backend.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockDto ToStockDto(this Stock stock)
+        {
+            return new StockDto
+            {
+                Id = stock.Id,
+                Symbol = stock.Symbol,
+                CompanyName = stock.CompanyName,
+                Purchase = stock.Purchase,
+                LastDiv = stock.LastDiv,
+                Industry = stock.Industry,
+                MarketCap = stock.MarketCap
+            };
+        }
+
+        public static Stock ToStock(this CreateStockRequestDto stockRequest)
+        {
+            return new Stock
+            {
+                Symbol = stockRequest.Symbol,
+                CompanyName = stockRequest.CompanyName,
+                Purchase = stockRequest.Purchase,
+                LastDiv = stockRequest.LastDiv,
+                Industry = stockRequest.Industry,
+                MarketCap = stockRequest.MarketCap
+            };
+        }
+
+        //public static Stock ToStock(this UpdateStockRequestDto stockRequest)
+        //{
+        //    return new Stock
+        //    {
+        //        Symbol = stockRequest.Symbol,
+        //        CompanyName = stockRequest.CompanyName,
+        //        Purchase = stockRequest.Purchase,
+        //        LastDiv = stockRequest.LastDiv,
+        //        Industry = stockRequest.Industry,
+        //        MarketCap = stockRequest.MarketCap
+        //    };
+        //}
+    }
+}
